e98acf191d7c78ac98e17752ccb75f902fc1c23c
Ahmed-Hmila <ahmed7hmila@gmail.com>
2025-01-05 15:31:31 +0100

    first commit

--- front-end/src/app/services/universite.service.ts
+++ front-end/src/app/services/universite.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, from, of } from 'rxjs';
-import { switchMap, shareReplay, tap, filter } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 interface Universite {
   idUniversite?: number;
@@ -13,53 +12,26 @@ interface Universite {
   providedIn: 'root'
 })
 export class UniversiteService {
-  private apiUrl$: Observable<string>;
+  private apiUrl = 'http://192.168.50.4:8089/tpfoyer/universite';
 
-  constructor(private http: HttpClient) {
-        // Share the result so that multiple calls don't trigger multiple loads of config
-        this.apiUrl$ = from(this.loadConfig()).pipe(
-          filter(config=> !!config),
-          tap(config=> console.log("config loaded: ", config)),
-          switchMap((config)=> of(config.apiUrl)),
-          shareReplay(1)
-        )
-  }
-  private async loadConfig(): Promise<any> {
-      try {
-          return await this.http.get<any>('assets/config.json').toPromise();
-        } catch (error) {
-            console.error('Error loading configuration file:', error);
-            return null; // or throw error to stop app bootstrap in your module
-        }
-  }
-
+  constructor(private http: HttpClient) {}
 
   getUniversites(): Observable<Universite[]> {
-    return this.apiUrl$.pipe(
-      switchMap(url=> this.http.get<Universite[]>(`${url}/retrieve-all-universites`))
-    )
+    return this.http.get<Universite[]>(`${this.apiUrl}/retrieve-all-universites`);
   }
 
   getUniversiteById(id: number): Observable<Universite> {
-    return this.apiUrl$.pipe(
-      switchMap(url=> this.http.get<Universite>(`${url}/retrieve-universite/${id}`))
-    )
+    return this.http.get<Universite>(`${this.apiUrl}/retrieve-universite/${id}`);
   }
   addUniversite(universite: Universite): Observable<Universite> {
-    return this.apiUrl$.pipe(
-      switchMap(url=>this.http.post<Universite>(`${url}/add-universite`, universite))
-    )
+    return this.http.post<Universite>(`${this.apiUrl}/add-universite`, universite);
   }
 
   updateUniversite(universite: Universite): Observable<Universite> {
-    return this.apiUrl$.pipe(
-      switchMap(url=> this.http.put<Universite>(`${url}/modify-universite`, universite))
-    )
+    return this.http.put<Universite>(`${this.apiUrl}/modify-universite`, universite);
   }
 
   deleteUniversite(id: number): Observable<void> {
-    return this.apiUrl$.pipe(
-      switchMap(url=>this.http.delete<void>(`${url}/remove-universite/${id}`))
-    )
+    return this.http.delete<void>(`${this.apiUrl}/remove-universite/${id}`);
   }
 }
